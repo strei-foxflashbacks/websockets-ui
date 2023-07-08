ac43d83ac6310b3aa5a3ebb832b32a9bc5eadd4d
ssekridov@gmail.com <ssekridov@gmail.com>
2023-07-08 11:26:13 +0200

    feat: dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
-// import * as fs from 'fs';
-// import * as path from 'path';
-// import * as http from 'http';
-
-// export const httpServer = http.createServer((req, res) => {
-//   const __dirname = path.resolve(path.dirname(''));
-//   const file_path =
-//     __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-//   fs.readFile(file_path, function (err, data) {
-//     if (err) {
-//       res.writeHead(404);
-//       res.end(JSON.stringify(err));
-//       return;
-//     }
-//     res.writeHead(200);
-//     res.end(data);
-//   });
-// });
-
 import WebSocket from 'ws';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = Number(process.env.PORT) || 8181;
 
 const ws = new WebSocket.Server({ port: HTTP_PORT }, () => {
   console.log(`⚡️[server]: Websocket server is running at http://localhost:${HTTP_PORT}`);
